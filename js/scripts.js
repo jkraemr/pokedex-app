9e44a170e7a863951877aa28e9bb39df23646427
Johannes Krämer <johannes.kraemer@hotmail.com>
2021-12-02 21:27:13 +0100

    Create pokemonList array with single Pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,86 @@
-alert('Hello Pokémon World');
-let favoriteFood = 'Pizza';
-favoriteFood = 'pikachuPizza!';
-document.write(favoriteFood);
+let pokemonList = [{
+  name: 'Bulbasaur',
+  height: 0.7,
+  weight: 6.9,
+  types: ['grass', 'poison']
+} {
+  name: 'Ivysaur',
+  height: 1,
+  weight: 13,
+  types: ['grass', 'poison']
+} {
+  name: 'Venusaur',
+  height: 2,
+  weight: 100,
+  types: ['grass', 'poison']
+} {
+  name: 'Charmander',
+  height: 0.6,
+  weight: 8.5,
+  types: ['fire']
+} {
+  name: 'Charmeleon',
+  height: 1.1,
+  weight: 19,
+  types: ['fire']
+} {
+  name: 'Charizard',
+  height: 1.7,
+  weight: 90.5,
+  types: ['fire', 'flying']
+} {
+  name: 'Squirtle',
+  height: 0.5,
+  weight: 9,
+  types: ['water']
+} {
+  name: 'Wartortle',
+  height: 1,
+  weight: 22.5,
+  types: ['water']
+} {
+  name: 'Blastoise',
+  height: 1.6,
+  weight: 85.5,
+  types: ['water']
+} {
+  name: 'Caterpie',
+  height: 0.3,
+  weight: 2.9,
+  types: ['bug']
+} {
+  name: 'Metapod',
+  height: 0.7,
+  weight: 9.9,
+  types: ['bug']
+} {
+  name: 'Butterfree',
+  height: 1.1,
+  weight: 32,
+  types: ['bug', 'flying']
+} {
+  name: 'Weedle',
+  height: 0.3,
+  weight: 3.2,
+  types: ['bug', 'poison']
+} {
+  name: 'Kakuna',
+  height: 0.6,
+  weight: 10,
+  types: ['bug', 'poison']
+} {
+  name: 'Beedrill',
+  height: 1,
+  weight: 29.5,
+  types: ['bug', 'poison']
+} {
+  name: 'Pidgey',
+  height: 0.3,
+  weight: 1.8,
+  types: ['flying', 'normal']
+} {
+  name: 'null',
+  height: null,
+  weight: null,
+  types: ['null', 'null']
+}];
