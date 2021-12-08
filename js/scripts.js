2ad790cc1de096bc172501429474b07ca9097080
Johannes Krämer <johannes.kraemer@hotmail.com>
2021-12-08 02:43:45 +0100

    Replace for loop with forEach function for pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -82,17 +82,36 @@ let pokemonList = [{
   types: ['flying', 'normal']
 }];
 
-// Create ordered list of pokemonList array objects
+// Create ordered list of pokemonList array objects with *for* loop
+// Add if else statements for indicating weight categories
+
+// document.write('<div class="main-content"><ol>');
+
+// for (let i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].weight > 30) {
+//     document.write('<li>' + pokemonList[i].name + ' (' + 'weight: ' + pokemonList[i].weight + 'kg)' + ' – OMG! That\'s a massive one. Beware: The mighty ' + pokemonList[i].name + ' is heavier than 30kg!' + '</li>');
+//   } else if ((pokemonList[i].weight >= 10) && (pokemonList[i].weight <= 30)) {
+//     document.write(`<li>${pokemonList[i].name} (weight: ${pokemonList[i].weight} kg) – WOW! The ${pokemonList[i].name} has a medium weight of 10-30kg.</li>`);
+//   } else {
+//     document.write(`<li>${pokemonList[i].name} (weight: ${pokemonList[i].weight} kg)</li>`);
+//   }
+// }
+
+// document.write('</ol></div>');
+
+// Create ordered list of pokemonList array objects with *forEach* function
 // Add if else statements for indicating weight categories
 
 document.write('<div class="main-content"><ol>');
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].weight > 30) {
-    document.write('<li>' + pokemonList[i].name + ' (' + 'weight: ' + pokemonList[i].weight + 'kg)' + ' – OMG! That\'s a massive one. Beware: The mighty ' + pokemonList[i].name + ' is heavier than 30kg!' + '</li>');
-  } else if ((pokemonList[i].weight >= 10) && (pokemonList[i].weight <= 30)) {
-    document.write(`<li>${pokemonList[i].name} (weight: ${pokemonList[i].weight} kg) – WOW! The ${pokemonList[i].name} has a medium weight of 10-30kg.</li>`);
+
+pokemonList.forEach(function(item) {
+  if (item.weight > 30) {
+    document.write('<li>' + item.name + ' (' + 'weight: ' + item.weight + 'kg)' + ' – OMG! That\'s a massive one. Beware: The mighty ' + item.name + ' is heavier than 30kg!' + '</li>');
+  } else if ((item.weight >= 10) && (item.weight <= 30)) {
+    document.write(`<li>${item.name} (weight: ${item.weight} kg) – WOW! The ${item.name} has a medium weight of 10-30kg.</li>`);
   } else {
-    document.write(`<li>${pokemonList[i].name} (weight: ${pokemonList[i].weight} kg)</li>`);
+    document.write(`<li>${item.name} (weight: ${item.weight} kg)</li>`);
   }
-}
+});
+
 document.write('</ol></div>');
