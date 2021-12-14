b996833b9086f177b3e8b00d2f836fcdf616ca71
Johannes Krämer <johannes.kraemer@hotmail.com>
2021-12-14 23:33:39 +0100

    Add loadList loadDetails functions to fetch from PokeAPI

--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,25 @@
 // Create IIFE to wrap pokemonList array with name, height, weight, and type properties
 let pokemonRepository = (function() {
-  let pokemonList = [{
-    name: 'Bulbasaur',
-    height: 0.7,
-    weight: 6.9,
-    types: ['grass', 'poison']
-  }, {
-    name: 'Ivysaur',
-    height: 1,
-    weight: 13,
-    types: ['grass', 'poison']
-  }, {
-    name: 'Venusaur',
-    height: 2,
-    weight: 100,
-    types: ['grass', 'poison']
-  }, {
-    name: 'Charmander',
-    height: 0.6,
-    weight: 8.5,
-    types: ['fire']
-  }, {
-    name: 'Charmeleon',
-    height: 1.1,
-    weight: 19,
-    types: ['fire']
-  }, {
-    name: 'Charizard',
-    height: 1.7,
-    weight: 90.5,
-    types: ['fire', 'flying']
-  }, {
-    name: 'Squirtle',
-    height: 0.5,
-    weight: 9,
-    types: ['water']
-  }, {
-    name: 'Wartortle',
-    height: 1,
-    weight: 22.5,
-    types: ['water']
-  }, {
-    name: 'Blastoise',
-    height: 1.6,
-    weight: 85.5,
-    types: ['water']
-  }, {
-    name: 'Caterpie',
-    height: 0.3,
-    weight: 2.9,
-    types: ['bug']
-  }, {
-    name: 'Metapod',
-    height: 0.7,
-    weight: 9.9,
-    types: ['bug']
-  }, {
-    name: 'Butterfree',
-    height: 1.1,
-    weight: 32,
-    types: ['bug', 'flying']
-  }, {
-    name: 'Weedle',
-    height: 0.3,
-    weight: 3.2,
-    types: ['bug', 'poison']
-  }, {
-    name: 'Kakuna',
-    height: 0.6,
-    weight: 10,
-    types: ['bug', 'poison']
-  }, {
-    name: 'Beedrill',
-    height: 1,
-    weight: 29.5,
-    types: ['bug', 'poison']
-  }, {
-    name: 'Pidgey',
-    height: 0.3,
-    weight: 1.8,
-    types: ['flying', 'normal']
-  }];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1118';
 
   // Return pokemonList array
   function getAll() {
     return pokemonList;
   }
 
-  // Add new validated Pokemon object to end of pokemonList array
+  // // Add new validated Pokemon object to end of pokemonList array
+  // function add(pokemon) {
+  //   if ((typeof pokemon === 'object') && ('name' in pokemon) && ('height' in pokemon) && ('weight' in pokemon)) {
+  //     pokemonList.push(pokemon);
+  //   } else {
+  //     console.log('incomplete data set')
+  //   }
+  // }
+
+  // Add new Pokemon object to end of pokemonList array
   function add(pokemon) {
-    if ((typeof pokemon === 'object') && ('name' in pokemon) && ('height' in pokemon) && ('weight' in pokemon)) {
-      pokemonList.push(pokemon);
-    } else {
-      console.log('incomplete data set')
-    }
+    pokemonList.push(pokemon);
   }
 
   // Generate pokemonList items for document with clickable buttons in unordered list
@@ -115,11 +41,49 @@ let pokemonRepository = (function() {
     console.log(pokemon);
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
   //  Return pokemonList objects for public functions outside of IIFE (getAll, add, addListItem)
   return {
     getAll: getAll,
     add: add,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   }
 
   // End of IIFE
@@ -127,15 +91,18 @@ let pokemonRepository = (function() {
 
 // Public functions
 
-// Add new object to pokemonList array within IIFE
-pokemonRepository.add({
-  name: 'Pidgeotto',
-  height: 1.1,
-  weight: 30,
-  types: ['flying', 'normal']
-});
+// // Add new object to pokemonList array within IIFE
+// pokemonRepository.add({
+//   name: 'Pidgeotto',
+//   height: 1.1,
+//   weight: 30,
+//   types: ['flying', 'normal']
+// });
 
 // Call addListItem function to render pokemonList items in document
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
