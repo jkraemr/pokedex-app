a48439a58b9580871ed1cd494437129612d147d9
Johannes Krämer <johannes.kraemer@hotmail.com>
2021-12-22 14:10:22 +0100

    Add Bootstrap utility classes d-flex btn for pokeList

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,10 @@ let pokemonRepository = (function() {
     let listItem = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = (pokemon.name);
-    button.classList.add('button');
+    button.classList.add('btn');
+    button.classList.add('btn-outline-primary');
+    listItem.classList.add('flex-fill');
+    listItem.classList.add('p-1');
     listItem.appendChild(button);
     pokeList.appendChild(listItem);
     button.addEventListener('click', function() {
